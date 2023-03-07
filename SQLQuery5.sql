88675d13553050984262e864f7c168bb418f3f8d
eddyangelop <eddysoullive@gmail.com>
2023-03-07 17:14:41 -0300

    implementação de agrupamento e soma

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -5,6 +5,23 @@ SELECT * FROM tb_ProdutoVenda
 SELECT * FROM tb_Produtos
 
 --Seleção dos dados com valores inner join
-SELECT P.cdProduto, P.nmProduto, PV.qtProduto, P.vlProduto 
+SELECT  V.cdVenda, C.nmCliente, P.cdProduto, P.nmProduto, PV.qtProduto, P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
 FROM tb_Produtos  P INNER JOIN tb_ProdutoVenda PV
 ON P.cdProduto = PV.cdProduto
+INNER JOIN tb_Vendas V ON PV.cdVenda = V.cdVenda
+INNER JOIN tb_Clientes C ON V.cdCliente = C.cdCliente
+
+
+--SOMA DOS VALORES
+SELECT C.nmCliente, SUM(PV.qtProduto) AS qtTotalProdutos, P.nmProduto, SUM(P.vlProduto * PV.qtProduto) AS qtValorTotalVenda--, P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
+FROM tb_Produtos  P INNER JOIN tb_ProdutoVenda PV ON P.cdProduto = PV.cdProduto
+INNER JOIN tb_Vendas V ON PV.cdVenda = V.cdVenda
+INNER JOIN tb_Clientes C ON V.cdCliente = C.cdCliente
+GROUP BY C.nmCliente, P.nmProduto
+
+--VALOR TOTAL
+SELECT C.nmCliente, SUM(PV.qtProduto) AS qtTotalProdutos, SUM(P.vlProduto * PV.qtProduto) AS qtValorTotalVenda--, P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
+FROM tb_Produtos  P INNER JOIN tb_ProdutoVenda PV ON P.cdProduto = PV.cdProduto
+INNER JOIN tb_Vendas V ON PV.cdVenda = V.cdVenda
+INNER JOIN tb_Clientes C ON V.cdCliente = C.cdCliente
+GROUP BY C.nmCliente
